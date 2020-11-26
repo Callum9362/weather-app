ba2a42d5c478b8ab160b5369d6af366c1fbafa28
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-27 09:27:18 +1100

    Added base html and css to app vue template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
     <main>
+      
       <div class="search-box">
         <input type="text" name="" id="" class="search-bar" placeholder="Search...">
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Cornwall, UK</div>
+          <div class="date">Monday, 21 January 2020</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°C</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
+
     </main>
   </div>
 </template>
@@ -35,7 +49,46 @@ body {
 }
 
 #app {
-  background-image: url('./assets/cold-bg.jpg')
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+ main {
+   min-height: 100vh;
+   padding: 25px;
+
+   background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75) )
+ }
+
+ .search-box {
+   width: 100%;
+   margin-bottom: 30px;
+ }
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
 }
 
 </style>
