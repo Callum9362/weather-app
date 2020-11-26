4df8cce3b1c1d8df55576e270209f5d32ce30285
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-27 09:09:44 +1100

    bootstrapped the project with assets, basic styling and api key

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" name="" id="" class="search-bar" placeholder="Search...">
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+const API_KEY = process.env.VUE_APP_API_KEY;
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      api_key: API_KEY
+    }
+  }
+}
+</script>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+#app {
+  background-image: url('./assets/cold-bg.jpg')
+}
+
+</style>
